8b8aae130bf19b7967745a779f739872a763bdcc
José Antonio González <jagonzalr@gmail.com>
2016-03-01 13:06:40 +0200

    add gulp tasks

---
+++ gulp-tasks/scripts.js
@@ -0,0 +1,27 @@
+
+'use strict';
+
+var babelify    = require('babelify'),
+    browserify  = require('browserify'),
+    fs          = require('fs'),
+    gulp        = require('gulp'),
+    rename      = require("gulp-rename"),
+    uglify      = require('gulp-uglify')
+
+/*
+* Javascript
+*/
+
+gulp.task("babelify", function () {
+    return browserify('src/main.jsx')
+        .transform(babelify, {presets: ["react"]})
+        .bundle()
+        .pipe(fs.createWriteStream('dev/js/main.js'));
+});
+
+gulp.task('minify-js', function() {
+    return gulp.src('dev/js/*.js')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'))
+});
